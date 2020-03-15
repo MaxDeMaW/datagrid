ea2968451f1f19010e632ea5259419fed0636336
Maxim Demenkow <demenkow.maxim@gmail.com>
2020-03-16 01:35:53 +0300

    feat: add sticky table header and sticky left column

--- datagrid/src/components/datagrid/datagrid.js
+++ datagrid/src/components/datagrid/datagrid.js
@@ -4,25 +4,37 @@ import { bindActionCreators } from 'redux';
 import * as actions from '../../utils/actions';
 import { connect } from 'react-redux';
 
+const onCkeckPerson = (event) => {
+  if (event.target.className === 'demo-icon icon-check-empty')
+    console.log(event.target.className);
+
+}
+
 const DataGrid = ({ allVisiblePersons, DeleteElement, SortByExperience }) => {
   const persons = allVisiblePersons.map(item => {
     const { id, ...itemProps } = item;
     return (
-      <div key={id} className="person">
-        <div className="person__name">{item.name}</div>
-        <div className="person__email">{item.email}</div>
-        <div className="person__city">{item.city}</div>
-        <div className="person__phone">{item.phone}</div>
-        <div className="person__birthday">{item.birthday}</div>
-        <div className="person__experience">{item.experience}</div>
-        <div className="person__isMarried">{item.isMarried.toString()}</div>
+      <div key={id} id={id} className="person">
+        <div><i className="demo-icon icon-check-empty" onClick={(e) => onCkeckPerson(e)}></i></div>
+        <div className="person__name">{itemProps.name}</div>
+        <div className="person__email">{itemProps.email}</div>
+        <div className="person__city">{itemProps.city}</div>
+        <div className="person__phone">{itemProps.phone}</div>
+        <div className="person__birthday">{itemProps.birthday}</div>
+        <div className="person__experience">{itemProps.experience}</div>
+        <div className="person__isMarried">{itemProps.isMarried.toString()}</div>
       </div>
     );
   });
 
-  return <div>
+  return <div className="tableContent">
     <div key={-1} className="person_header">
-      <div className="person__name"><div>Имя</div><div><i className="demo-icon icon-angle-circled-up" onClick={DeleteElement}></i><i className="demo-icon icon-angle-circled-down"></i></div></div>
+      <div><i className="demo-icon icon-check-empty"></i></div>
+      <div className="person__name">
+        <div>Имя</div>
+        <div><i className="demo-icon icon-angle-circled-up" onClick={DeleteElement}></i><i className="demo-icon icon-angle-circled-down"></i></div>
+      </div>
+
       <div className="person__email">E-mail</div>
       <div className="person__city">City</div>
       <div className="person__phone">Phone</div>
